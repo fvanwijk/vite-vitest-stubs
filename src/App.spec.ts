7b2df4884639914a758fc9891ad125033b28dae0
Frank van Wijk <info@frankvanwijk.nl>
2022-08-09 21:46:33 +0200

    Rewrite test using vue-test-utils

--- src/App.spec.ts
+++ src/App.spec.ts
@@ -1,17 +1,20 @@
-import { describe, it } from "vitest";
-import { render, screen } from "@testing-library/vue";
+import { describe, expect, it } from "vitest";
+import { mount } from "@vue/test-utils";
 import App from "./App.vue";
 
 describe("Example test to show that stubs are broken", () => {
   it("should not stub HelloWorld when unplugin-vue-components is used in vite.config.ts", () => {
-    render(App, {
+    const wrapper = mount(App, {
       global: {
         stubs: {
-          HelloWorld: { props: ["msg"], template: "HELLO WORLD STUB" },
+          HelloWorld: {
+            props: ["msg"],
+            template: '<span id="hello">HELLO WORLD STUB</span>',
+          },
         },
       },
     });
 
-    screen.getByText("HELLO WORLD STUB");
+    expect(wrapper.find("#hello").text()).to.equals("HELLO WORLD STUB");
   });
 });
